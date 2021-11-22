c49046acfc6f896c430ea7f75c5ec59df072c900
Giuseppe <90353257+Mithrandir90@users.noreply.github.com>
2021-11-22 18:10:33 +0000

    ingredient and fridge item models created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_170746) do
+ActiveRecord::Schema.define(version: 2021_11_22_180900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "fridge_items", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.date "expiry_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_fridge_items_on_ingredient_id"
+    t.index ["user_id"], name: "index_fridge_items_on_user_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "unit"
+    t.integer "calories"
+    t.float "price"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +49,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_170746) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "fridge_items", "ingredients"
+  add_foreign_key "fridge_items", "users"
 end
