cea02ac901c6fd2daea004a8f72f344a3829af6d
Giuseppe <90353257+Mithrandir90@users.noreply.github.com>
2021-11-23 14:10:24 +0000

    validations removed

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,10 +1,4 @@
 class Ingredient < ApplicationRecord
   belongs_to :fridge_item
   belongs_to :recipe_item
-
-  validates :name, presence: true, length: { minimum: 2 }
-  validates :unit, presence: true
-  validates :calories, presence: true
-  validates :price, presence: true
-  validates :category, presence: true
 end
