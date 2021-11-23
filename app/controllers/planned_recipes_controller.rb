895eee810679692713b21160a0e6fcbbf3643f74
Giuseppe <90353257+Mithrandir90@users.noreply.github.com>
2021-11-23 12:57:06 +0000

    Planned recipes controller create

--- app/controllers/planned_recipes_controller.rb
+++ app/controllers/planned_recipes_controller.rb
@@ -1,2 +1,45 @@
 class PlannedRecipesController < ApplicationController
+  before_action :find_planned_recipe, only: %i[show edit update]
+
+  def index
+    @planned_recipes = PlannedRecipe.all
+  end
+
+  def show
+  end
+
+  def new
+    @planned_recipe = PlannedRecipe.new
+  end
+
+  def create
+    @planned_recipe = PlannedRecipe.new(planned_recipe_params)
+    @planned_recipe.user_id = current_user.id
+    if @planned_recipe.save
+      redirect_to planned_recipes_path(@planned_recipe)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @planned_recipe.update(planned_recipe_params)
+      redirect_to planned_recipes_path(@planned_recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_planned_recipe
+    @planned_recipe = PlannedRecipe.find(params[:id])
+  end
+
+  def planned_recipe_params
+    params.require(:planned_recipe).permit(:date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :recipes
-  resources :planned_recipes, only: [:create, :index, :edit, :update]
+  resources :planned_recipes, only: [:create, :index, :edit, :update, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :fridge_items, path: "my_fridge", only: [:index, :new, :create, :edit, :update, :destroy]
 end
