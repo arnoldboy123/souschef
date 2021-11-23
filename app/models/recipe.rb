d06856bd4ee465e30949683a7e76fc0a446c331a
thomas flaherty <thomasflaherty990@gmail.com>
2021-11-23 14:51:14 +0000

    recipe validations added

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_many :recipe_items, :planned_recipes, :reviews
+  validates :creator, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :instructions, presence: true
+  validates :cooking_time, presence: true, numerically: { only_integer: true }
 end
