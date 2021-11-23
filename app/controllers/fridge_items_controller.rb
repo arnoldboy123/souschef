9abc82c9079a3380686fb366eddaf05d20fdf247
Alex Boxell <alexjboxell@gmail.com>
2021-11-23 11:59:07 +0000

    added strong params and find

--- app/controllers/fridge_items_controller.rb
+++ app/controllers/fridge_items_controller.rb
@@ -1,2 +1,21 @@
 class FridgeItemsController < ApplicationController
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_fridge_item
+    @fridge_item = Fridge.find(params[:id])
+  end
+
+  def fridge_params
+    params.require(:fridge_item).permit(:ingredient_id, :expiry_date, :user_id, :quantity)
+  end
 end
