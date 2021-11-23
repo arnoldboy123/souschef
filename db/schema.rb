64b33058a64f42ab3737f998cb6c5a420d8bb852
Alex Boxell <alexjboxell@gmail.com>
2021-11-23 10:40:22 +0000

    changed FriendRequest model to CamelCase

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_180900) do
+ActiveRecord::Schema.define(version: 2021_11_23_103756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "FriendRequests", force: :cascade do |t|
+    t.boolean "approved"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "requester_id", null: false
+    t.bigint "recipient_id", null: false
+    t.index ["recipient_id"], name: "index_FriendRequests_on_recipient_id"
+    t.index ["requester_id"], name: "index_FriendRequests_on_requester_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.string "content"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "fridge_items", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -33,27 +54,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_180900) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "friend_requests", force: :cascade do |t|
-    t.boolean "approved"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "requester_id", null: false
-    t.bigint "recipient_id", null: false
-    t.index ["recipient_id"], name: "index_friend_requests_on_recipient_id"
-    t.index ["requester_id"], name: "index_friend_requests_on_requester_id"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "post_id", null: false
-    t.string "content"
-    t.date "date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -78,11 +78,11 @@ ActiveRecord::Schema.define(version: 2021_11_22_180900) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "fridge_items", "ingredients"
-  add_foreign_key "fridge_items", "users"
-  add_foreign_key "friend_requests", "users", column: "recipient_id"
-  add_foreign_key "friend_requests", "users", column: "requester_id"
+  add_foreign_key "FriendRequests", "users", column: "recipient_id"
+  add_foreign_key "FriendRequests", "users", column: "requester_id"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "fridge_items", "ingredients"
+  add_foreign_key "fridge_items", "users"
   add_foreign_key "posts", "users"
 end
