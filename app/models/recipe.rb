b66ddf366d00d6b959419c6ab6033056b5953bd0
Giuseppe <90353257+Mithrandir90@users.noreply.github.com>
2021-11-23 15:42:06 +0000

    space removed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,10 @@ class Recipe < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :recipe_items, :planned_recipes, :reviews
   has_many :ingredients, through: :recipe_items
-  
-  validates :creator, presence: true
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :instructions, presence: true
-  validates :cooking_time, presence: true, numerically: { only_integer: true }
+
+  # validates :creator, presence: true
+  # validates :name, presence: true
+  # validates :description, presence: true
+  # validates :instructions, presence: true
+  # validates :cooking_time, presence: true, numerically: { only_integer: true }
 end
