f9e24083db4e9126a8dfc7cc4f20db73d0a9e553
Alex Boxell <alexjboxell@gmail.com>
2021-12-01 16:28:13 +0000

    added stars for review, among other things

---
+++ app/javascript/plugins/init_star_rating.js
@@ -0,0 +1,13 @@
+import "jquery-bar-rating";
+
+const initStarRating = () => {
+  $('#review_rating').barrating({
+    theme: 'css-stars',
+    onSelect: (value, text, event) => {
+      const form = $("form.review_form");
+      form.submit();
+    }
+  });
+};
+
+export { initStarRating };
