8b9fcadc654051be22d1c9c39c6e5139036362ae
Alex Boxell <alexjboxell@gmail.com>
2021-12-02 13:38:33 +0000

    validate ingredient name

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,8 +1,10 @@
 class Ingredient < ApplicationRecord
+  validates :name, uniqueness: true
   include PgSearch::Model
   pg_search_scope :search_ingredients,
     against: %i[name category],
     using: {
       tsearch: { prefix: true }
   }
+
 end
